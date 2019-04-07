0cef95db1301bca7952af9cfe99ef5e076e25f7c
shilangyu <xmarcinmarcin@gmail.com>
2019-04-07 14:36:50 +0200

    more broad support for iterators

--- app.ts
+++ app.ts
@@ -5,23 +5,28 @@ type Props = {
 	amount: number
 	template: string
 	outFile?: fs.PathLike
-	generators: { [key: string]: Iterator<string> }
+	iterators: { [key: string]: () => Iterator<string> }
 }
 
-export default ({ amount, template, outFile = 'out.txt', generators }: Props): string => {
+export default ({ amount, template, outFile = 'out.txt', iterators }: Props): string => {
 	const outputs = new Array<string>(amount)
+
+	const initIters: { [key: string]: Iterator<string> } = {}
+	for (const key of Object.keys(iterators)) {
+		initIters[key] = iterators[key]()
+	}
 
 	for (const i of range(amount)) {
 		let curr = ''
-		for (const key of Object.keys(generators)) {
-			curr = template.split('${generator.' + key + '}').join(generators[key].next().value)
+		for (const key of Object.keys(initIters)) {
+			curr = template.split('${iterator.' + key + '}').join(initIters[key].next().value)
 		}
 		outputs[i] = curr
 	}
 
 	const {
 		groups: { ext }
-	} = (outFile as string).match(/\.(?<ext>\w+)^/) as { groups: { [key: string]: string } }
+	} = (outFile as string).match(/\.(?<ext>\w+)$/) as { groups: { [key: string]: string } }
 
 	const result = ext === 'json' ? JSON.stringify(outputs) : outputs.join('\n')
 	fs.writeFileSync(outFile, result)
