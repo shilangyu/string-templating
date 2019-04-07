9d5e4d884d4bbd18e6a17e61e337c257206f9ec2
shilangyu <xmarcinmarcin@gmail.com>
2019-04-07 14:46:55 +0200

    added file test

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -26,6 +26,26 @@ i should be incrementing: 3
 i should be incrementing: 4
 i should be incrementing: 5`
 
+		expect(result).toBe(expected)
+	})
+
+	it('tests if it creates a file', () => {
+		StrTempl({
+			amount: 5,
+			template: 'i should be incrementing: ${iterator.inc}',
+			iterators: {
+				inc: function*() {
+					for (let i = 1; i <= 5; i++) {
+						yield '' + i
+					}
+				}
+			},
+			outFile: 'out.txt'
+		})
+
+		const result = fs.existsSync('out.txt')
+		const expected = true
+
 		cleanFile('out.txt')
 
 		expect(result).toBe(expected)
