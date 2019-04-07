326d17c96eea6882839e5db2c94c1686edc755d8
shilangyu <xmarcinmarcin@gmail.com>
2019-04-07 14:40:42 +0200

    created tests

---
+++ tests/app.test.ts
@@ -0,0 +1,33 @@
+import StrTempl from '../app'
+import * as fs from 'fs'
+
+const cleanFile = (path: fs.PathLike) => {
+	if (fs.existsSync(path)) {
+		fs.unlinkSync(path)
+	}
+}
+
+describe('app', () => {
+	it('tests if returned value is correct', () => {
+		const result = StrTempl({
+			amount: 5,
+			template: 'i should be incrementing: ${iterator.inc}',
+			iterators: {
+				inc: function*() {
+					for (let i = 1; i <= 5; i++) {
+						yield '' + i
+					}
+				}
+			}
+		})
+		const expected = `i should be incrementing: 1
+i should be incrementing: 2
+i should be incrementing: 3
+i should be incrementing: 4
+i should be incrementing: 5`
+
+		cleanFile('out.txt')
+
+		expect(result).toBe(expected)
+	})
+})
